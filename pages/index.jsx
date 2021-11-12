70e6e9f74b15e881cf3b811cf734afe5997f2278
alessandrordgs <ale.silva1120@gmail.com>
2021-11-12 17:59:32 -0300

    calculadora feita, falta css

---
+++ pages/index.jsx
@@ -0,0 +1,62 @@
+import styles from "../styles/Home.module.css";
+import { useState } from "react";
+
+export default function Home() {
+  const [a, setA] = useState();
+  const [b, setB] = useState();
+  const [c, setC] = useState();
+  const [delta, setDelta] = useState();
+  const [x1, setX1] = useState();
+  const [x2, setX2] = useState();
+  function handleForm(e) {
+    e.preventDefault();
+    const delta = b ** 2 - 4 * a * c;
+    const raizDelta = Math.sqrt(delta);
+    const x1 = (parseInt(-b) + raizDelta) / (2 * parseInt(a));
+    const x2 = (parseInt(-b) - raizDelta) / (2 * parseInt(a));
+    setDelta(delta);
+    setX1(x1);
+    setX2(x2);
+  }
+  return (
+    <div className={styles.container}>
+      <h1>Calculadora de Bhaskara</h1>
+      <main className={styles.main}>
+        <div>
+          <form onSubmit={handleForm} action="">
+            <div>
+              <p>Coloque o A</p>
+              <input type="number" onChange={(e) => setA(e.target.value)} />
+            </div>
+            <div>
+              <p>Coloque o B</p>
+              <input type="number" onChange={(e) => setB(e.target.value)} />
+            </div>
+            <div>
+              <p>Coloque o C</p>
+              <input type="number" onChange={(e) => setC(e.target.value)} />
+            </div>
+            <button type="submit">Calcular</button>
+          </form>
+
+          <div>
+            <p>a : {a}</p>
+            <p>b : {b}</p>
+            <p>c : {c}</p>
+          </div>
+        </div>
+        <div>
+          <p>Delta : {delta < 0 ? "o valor é negativo" : delta}</p>
+          {delta < 0 ? (
+            <p>Não é possivel continuar com a operação, informe novo valores</p>
+          ) : (
+            <>
+              <p>x &prime; : {x1}</p>
+              <p>x&prime;&prime; : {x2}</p>
+            </>
+          )}
+        </div>
+      </main>
+    </div>
+  );
+}
