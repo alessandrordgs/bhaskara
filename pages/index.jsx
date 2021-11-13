a0c5fcb8d53ba06332e0a1351ac4e4c585e44e3a
alessandrordgs <ale.silva1120@gmail.com>
2021-11-12 22:40:27 -0300

    testando useState com hook

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,22 +2,35 @@ import styles from "../styles/Home.module.css";
 import { useState } from "react";
 
 export default function Home() {
-  const [a, setA] = useState();
-  const [b, setB] = useState();
-  const [c, setC] = useState();
-  const [delta, setDelta] = useState();
-  const [x1, setX1] = useState();
-  const [x2, setX2] = useState();
+  // const [a, setA] = useState();
+  // const [b, setB] = useState();
+  // const [c, setC] = useState();
+  // // const [delta, setDelta] = useState();
+  // const [x1, setX1] = useState();
+  // const [x2, setX2] = useState();
+
+  const [values, setValues] = useState({
+    a:0,
+    b:0,
+    c:0,
+    delta:0,
+    x1:0, 
+  })
+
   function handleForm(e) {
     e.preventDefault();
     const delta = b ** 2 - 4 * a * c;
     const raizDelta = Math.sqrt(delta);
     const x1 = (parseInt(-b) + raizDelta) / (2 * parseInt(a));
     const x2 = (parseInt(-b) - raizDelta) / (2 * parseInt(a));
-    setDelta(delta);
+    setValues({
+      delta : 2
+    })
     setX1(x1);
     setX2(x2);
   }
+
+  console.log(values.delta)
   return (
     <div className={styles.container}>
       <h1>Calculadora de Bhaskara</h1>
@@ -26,7 +39,7 @@ export default function Home() {
           <form onSubmit={handleForm} action="">
             <div>
               <p>Coloque o A</p>
-              <input type="number" onChange={(e) => setA(e.target.value)} />
+              <input type="number" onChange={(e) => setValues({a:e.target.value})} />
             </div>
             <div>
               <p>Coloque o B</p>
@@ -37,16 +50,19 @@ export default function Home() {
               <input type="number" onChange={(e) => setC(e.target.value)} />
             </div>
             <button type="submit">Calcular</button>
+            {values.a}
           </form>
 
           <div>
-            <p>a : {a}</p>
+            {/* <p>a : {a}</p>
             <p>b : {b}</p>
-            <p>c : {c}</p>
+            <p>c : {c}</p> */}
           </div>
         </div>
         <div>
-          <p>Delta : {delta < 0 ? "o valor é negativo" : delta}</p>
+
+
+          {/* <p>Delta : {delta < 0 ? "o valor é negativo" : delta}</p>
           {delta < 0 ? (
             <p>Não é possivel continuar com a operação, informe novo valores</p>
           ) : (
@@ -54,7 +70,7 @@ export default function Home() {
               <p>x &prime; : {x1}</p>
               <p>x&prime;&prime; : {x2}</p>
             </>
-          )}
+          )} */}
         </div>
       </main>
     </div>
